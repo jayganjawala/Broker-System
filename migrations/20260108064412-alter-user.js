6a8eb6f31d71484509aff5873def0496368583a9
jayganjawala <jayganjawala.it22@scet.ac.in>
2026-01-08 14:26:56 +0530

    Commit

--- migrations/20260108064412-alter-user.js
+++ migrations/20260108064412-alter-user.js
@@ -6,7 +6,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-  
     await queryInterface.addConstraint('Users', {
       fields: ['employeeId'],
       type: 'foreign key',
